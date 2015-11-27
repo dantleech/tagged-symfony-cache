f7d9f2c3d669d714d4e094fb509d35bcfe2def64
dantleech <dan.t.leech@gmail.com>
2015-11-27 09:30:37 +0100

    NullTagManager

---
+++ lib/NullTagManager.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace DTL\TaggedHttpCache;
+
+class TagManager implements TagManagerInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getPathsForTag($tag)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function invalidateTags(array $tags)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createTag($tag, $contentDigest)
+    {
+    }
+}
